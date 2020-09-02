6bf88398010486a12d73d35cb809b2d6a4385944
Patrick Dean <patrick.l.dean@gmail.com>
2020-09-02 09:11:24 -0400

    Add page number to every page

--- assemble.py
+++ assemble.py
@@ -11,6 +11,11 @@ files = sorted(
 
 output = "\n\n\\page\n\n".join(map(lambda x: x.content, files))
 
+auto_page_div = "<div class='pageNumber auto'></div>"
+
+output = f"\n\n{auto_page_div}\n\n\\page\n\n".join(output.split("\\page")) + f"\n\n{auto_page_div}\n\n"
+
+
 with open("dist/output.md", "w") as f:
     f.write(output)
 
