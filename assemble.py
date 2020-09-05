fd10fd5a4108acf84284e3768a598e6c015af504
Patrick Dean <patrick.l.dean@gmail.com>
2020-09-05 00:31:25 -0400

    Add css, and render markdown to html

--- assemble.py
+++ assemble.py
@@ -1,8 +1,14 @@
 import frontmatter
 import glob
 from pathlib import Path
+import markdown
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(loader=FileSystemLoader("./templates"), autoescape=select_autoescape(["html", "xml"]))
 
 Path("./dist").mkdir(parents=True, exist_ok=True)
+
+template = env.get_template("template.html.j2")
 
 files = sorted(
     filter(lambda x: x.get("order"), (frontmatter.load(file) for file in glob.glob("./*.md"))),
@@ -22,9 +28,22 @@ outputs = iter(map(render, files))
 
 output = f"\n\n\\page\n\n".join(outputs)
 
+pages = output.split("\\page")
+
+html_pages = []
+
+for page in pages:
+    html_pages.append(markdown.markdown(page, extensions=["markdown.extensions.tables"]))
+
+render = template.render(pages=html_pages)
+
 
 with open("dist/output.md", "w") as f:
     f.write(output)
+
+
+with open("dist/output.html", "w") as f:
+    f.write(render)
 
 
 if __name__ == "__main__":
